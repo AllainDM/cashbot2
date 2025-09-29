323d78076b71051a36d1a8f94c907998a4a7ad91
allaindm <allaindemoupu@mail.ru>
2025-09-29 21:34:31 +0300

    Инициализация бота

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
+import os
+import asyncio
+import logging
+
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+import config
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+BOT_API_TOKEN = os.getenv("BOT_TOKEN")
+
+# Инициализация бота и диспетчера
+bot = Bot(token=BOT_API_TOKEN)
+dp = Dispatcher()
+
+# Тестовый обработчик команды /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    # Узнаем ид пользователя.
+    user_id = message.from_user.id
+    # Авторизация
+    if user_id in config.USERS:
+        logger.info(f"Запрос от пользователя {user_id}")
+        await message.answer("Привет! Я бот...")
 
 
-def main():
-    ...
+# Основная функция запуска бота
+async def main():
+    # Удаляем вебхук, если он был установлен
+    await bot.delete_webhook(drop_pending_updates=True)
 
-
+    # Запускаем поллинг
+    logger.info("Бот запущен")
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
