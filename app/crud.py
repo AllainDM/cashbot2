dc071fd358b0b19de74f4f9e848b47174c7260e1
allaindm <allaindemoupu@mail.ru>
2025-10-02 23:35:40 +0300

    Фикс добавление записи в БД, правильный вызов соединения с БД

--- app/crud.py
+++ app/crud.py
@@ -21,29 +21,36 @@ async def add_note(user_tg_id: int, category: str, sub_category: str, summ: str 
     """
     logger.info("Запуск асинхронной функции add_note")
 
+    connection = await get_async_sqlite_session() # 1. Получаем соединение
+    if connection is None:
+        return False
+
     try:
         # Используем асинхронный контекстный менеджер 'async with',
         # который предоставляет функция get_async_sqlite_session.
         # Это обеспечивает получение и автоматическое закрытие/освобождение соединения.
         # async with await get_async_sqlite_session() as connection:
-        async with aiosqlite.connect(config.DATABASE_NAME) as connection:
+        # async with aiosqlite.connect(config.DATABASE_NAME) as connection:
 
-            date = datetime.now().strftime("%d.%m.%Y")  # Формат даты: день, месяц, год
+        date = datetime.now().strftime("%d.%m.%Y")  # Формат даты: день, месяц, год
 
-            # Асинхронное выполнение SQL-запроса
-            # В aiosqlite можно использовать .execute() прямо на объекте connection
-            await connection.execute(
-                "INSERT INTO out (user_tg_id, category, sub_category, summ, description, date) VALUES (?, ?, ?, ?, ?, ?)",
-                (user_tg_id, category, sub_category, summ, description, date)
-            )
+        # Асинхронное выполнение SQL-запроса
+        # В aiosqlite можно использовать .execute() прямо на объекте connection
+        await connection.execute(
+            "INSERT INTO out (user_tg_id, category, sub_category, summ, description, date) VALUES (?, ?, ?, ?, ?, ?)",
+            (user_tg_id, category, sub_category, summ, description, date)
+        )
 
-            # Асинхронное подтверждение транзакции (commit)
-            await connection.commit()
+        # Асинхронное подтверждение транзакции (commit)
+        await connection.commit()
 
-            logger.info(f"Запись успешно добавлена для пользователя ID: {user_tg_id}.")
-            return True
+        logger.info(f"Запись успешно добавлена для пользователя ID: {user_tg_id}.")
+        return True
 
     except Exception as ex:
         # Обработка любых исключений (например, ошибок БД или подключения)
         logger.error(f"Ошибка добавления данных в БД для пользователя ID {user_tg_id}: {ex}", exc_info=True)
         return False
+
+    finally:
+        await connection.close()
